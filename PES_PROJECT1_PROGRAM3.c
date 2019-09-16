fd0b0ec29b06a8aad3c1a927e8341b8d074c9992
HARSH1911RATHORE <55412102+HARSH1911RATHORE@users.noreply.github.com>
2019-09-16 16:52:08 -0600

    Add files via upload

--- PES_PROJECT1_PROGRAM3.c
+++ PES_PROJECT1_PROGRAM3.c
@@ -2,65 +2,80 @@
 void dec2bin(int Decimal);                      /*Function prototype to convert decimal to binary */
 int main()
 {
-    int a,input=0xCAFE,b,c,d,e,f,g,z,z1,z2;
+    int input=0xCAFE,check1,check2,check3,check4,operation1,operation2,operation3,byte_rev,rot4left,rot8right;
     printf("The input in Hexadecimal=");
     printf("0X%X\n",input);                     /*Print the Hexadecimal input*/
-    a=input&0xF;                                /*Bitwise and operation */
-    if (a==0X7||a==0XB||a==0XD||a==0XE)         /*check if 3 of the last 4 bits are on */
+    check1=input&0xF;                                /*Bitwise and operation  to check if any last 3 bits of last 4 bits are on*/
+    if (check1==0X7||check1==0XB||check1==0XD||check1==0XE)         /*check if 3 of the last 4 bits are on and print true if yes and false if not*/
     {
-        printf("TRUE that 3 of the last 4 bits are on \t");                      /*if 3 of the last 4 bits are on print True and then print the binary value*/
-        printf("The binary value=");
-        printf("0b");
-        dec2bin(input);                           /*Function call to convert decimal to binary */
-        printf("\n");                              /*goto next line*/
-        printf("\nThe Reverse of the byte order in Hexadecimal=");
-        b=((input>>8)|(input<<8));                 /*Reverse the byte order  by shifting the input bitwise left and right by 8 bits and bitwise or*/
-        z=b&0x0000FFFF;                            /* Bitwise or with  0x0000FFFF to get the required output*/
-        printf("0X%X\n",z);
-        c=input&0XF;
-        if (c==0X7||c==0XB||c==0XD||c==0XE)
-        {
-            printf("TRUE that 3 of the last 4 bits are on \t");
-            printf("The binary value=");
-            printf("0b");
-            dec2bin(z);                        /*Function call to convert decimal to binary */
-            printf("\n");
-            printf("\nThe Rotation of result by 4 bits to the left in Hexadecimal=");
-            d=((b<<4)|(b>>12));                /*Rotate bits left by 4 bits by shifting the input bitwise left and right by 4 and bits respectively and bitwise or*/
-            z1=d&0x0000FFFF;
-            printf("0X%X\n",z1);
-            e=input&0XF;
-            if (e==0X7||e==0XB||e==0XD||e==0XE) /*check if 3 of the last 4 bits are on */
-            {
-                printf("TRUE that 3 of the last 4 bits are on \t");
-                printf("The binary value=");
-                printf("0b");
-                dec2bin(z1);                         /*Function call to convert decimal to binary */
-                printf("\n");
-                printf("\nThe Rotation of result by 8 bits to the right in Hexadecimal=");
-                f=((d>>8)|(d<<8));              /*Rotate bits right by 8 bits by shifting the input bitwise left and right by 8 and bitwise or*/
-                z2=f&0x0000FFFF;
-                printf("0X%X\n",z2);
-                g=input&0XF;
-
-                if (g==0X7||g==0XB||g==0XD||g==0XE) /*check if 3 of the last 4 bits are on */
-				{
-                    printf("TRUE that 3 of the last 4 bits are on \t");
-                    printf("The binary value=");
-                    printf("0b");
-                    dec2bin(z2);                     /*Function call to convert decimal to binary */
-                    printf("\n");
-				}
-            }
-        }
-
-
+        printf("TRUE that only 3 of the last 4 bits are on \t");
     }
-
     else
     {
-        printf("FALSE");
+        printf("FALSE as 3 of the last 4 bits are off \t");
     }
+    printf("The binary value=");                             /* print the binary value */
+    printf("0b");
+    dec2bin(input);                                                    /*Function call to convert decimal to binary */
+    printf("\n");                                                /*goto next line*/
+    printf("\nThe Reverse of the byte order in Hexadecimal=");
+    operation1=((input>>8)|(input<<8));                        /*Reverse the byte order  by shifting the input bitwise left and right by 8 bits
+                                                                                and bitwise or*/
+    byte_rev=operation1&0x0000FFFF;                            /* Bitwise or with  0x0000FFFF to get the required output*/
+    printf("0X%X\n",byte_rev);
+    check2=byte_rev&0XF;                                      /*Bitwise and operation  to check if any last 3 bits of last 4 bits are on*/
+    if (check2==0X7||check2==0XB||check2==0XD||check2==0XE)    /*check if 3 of the last 4 bits are on and print true if yes and false if not*/
+    {
+        printf("TRUE that only 3 of the last 4 bits are on \t");
+    }
+    else
+    {
+        printf("FALSE as only 3 of the last 4 bits are not on \t");
+    }
+    printf("The binary value=");                            /* print the binary value */
+    printf("0b");
+    dec2bin(byte_rev);                                      /*Function call to convert decimal to binary */
+    printf("\n");
+    printf("\nThe Rotation of result by 4 bits to the left in Hexadecimal=");
+    operation2=((byte_rev<<4)|(byte_rev>>12));                /*Rotate bits left by 4 bits by shifting the input bitwise
+                                                                                    left and right by 4 and bits respectively and bitwise or*/
+    rot4left=operation2&0x0000FFFF;
+    printf("0X%X\n",rot4left);
+    check3=rot4left&0XF;                                       /*Bitwise and operation  to check if any last 3 bits of last 4 bits are on*/
+    if (check3==0X7||check3==0XB||check3==0XD||check3==0XE) /*check if 3 of the last 4 bits are on and print true if yes and false if not*/
+    {
+        printf("TRUE that only 3 of the last 4 bits are on \t");
+    }
+    else
+    {
+        printf("FALSE as only 3 of the last 4 bits are not on  \t");
+    }
+    printf("The binary value=");                             /* print the binary value */
+    printf("0b");
+    printf("0b");
+    dec2bin(rot4left);                                      /*Function call to convert decimal to binary */
+    printf("\n");
+    printf("\nThe Rotation of result by 8 bits to the right in Hexadecimal=");
+    operation3=((rot4left>>8)|(rot4left<<8));              /*Rotate bits right by 8 bits by shifting
+                                                                                the input bitwise left and right by 8 and bitwise or*/
+    rot8right=operation3&0x0000FFFF;
+    printf("0X%X\n",rot8right);
+    check4=rot8right&0XF;                                       /*Bitwise and operation  to check if any last 3 bits of last 4 bits are on*/
+
+    if (check4==0X7||check4==0XB||check4==0XD||check4==0XE) /*check if 3 of the last 4 bits are on and print true if yes and false if not*/
+    {
+        printf("TRUE that only 3 of the last 4 bits are on \t");
+    }
+    else
+    {
+        printf("FALSE as only 3 of the last 4 bits are not on \t");
+    }
+    printf("The binary value=");                            /* print the binary value */
+    printf("0b");
+    dec2bin(rot8right);                                    /*Function call to convert decimal to binary */
+    printf("\n");
+
+
     return 0;
 
 }
@@ -75,7 +90,8 @@ void dec2bin(int Decimal)                            /*Function declaration to c
     while (length)                                   /*while length of binary bits remains more than 1 while loop executes*/
     {
         if(Decimal&i)                                /*Checking if bitwise and of Decimal Value and i is 1 */
-            bin[j--] = '1';                          /*If the bitwise operation value is less than 1, then print 1 at end of array else print 0*/
+            bin[j--] = '1';                          /*If the bitwise operation value is less than 1,
+                                                        then print 1 at end of array else print 0*/
         else
             bin[j--] = '0';
         i = i << 1;                                 /*shift i by 1 in each loop iteration and do bitwise operation everytime*/
