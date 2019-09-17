91e87ddde53751eccf63087cfb88da5d0d0a7db6
HARSH1911RATHORE <55412102+HARSH1911RATHORE@users.noreply.github.com>
2019-09-17 12:24:49 -0600

    Add files via upload

--- PES_PROJECT1_PROGRAM1.h
+++ PES_PROJECT1_PROGRAM1.h
@@ -1,14 +1,3 @@
-/**
- *@file PES_PROJECT1_PROGRAM1.h
- *@brief Declaration of all inbuilt library functions,
- *function prototypes which are there in the c source file 
- *used online gdb,code blocks and gcc development environment on windows 
- *for compiling,debugging, running the program
- *@author Harsh Rathore 
- *@date September 14 2019 
- *@version 1.0
- *
- */ 
 #ifndef PES_PROJECT1_PROGRAM1
 #define PES_PROJECT1_PROGRAM1
 
@@ -17,116 +6,116 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-/**
-*@brief finds decimal equivalent of a number
-*for a following radix value
-*Input value which needs to converted
-*
-*@param radix value
-*@param numerical input value to be converted 
-*
-*@return int
+
+/**​​
+*​​@brief​​ finds decimal equivalent of a number
+​​*for a following radix value
+​​*​​​Input value which needs to converted
+​​*
+​​*​​@param​​ radix value
+​​*​​@param​​ numerical input value​ ​to be converted ​
+​*
+​​*​​@return​​ int​
 */
 int32_t dec (int32_t r, int32_t n);
-
-/**
-*@brief converts decimal equivalent of input number into binary form
-*
-*Decimal value which needs to be converted
-*
-*@param Decimal value 
-*@param op_size value to write to the location
-*
-*@return void
+/**​​
+*​​@brief​​ converts decimal equivalent of input number into binary form
+​​*
+​​*​​​Decimal value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Decimal value
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ void.​
 */
-void dec2bin (int32_t Decimal, int32_t op_size);
+void dec2bin (int Decimal, int op_size);
 
-/**
-*@brief converts decimal equivalent of number into octal form
-*
-*Decimal value which needs to be converted
-*
-*@param Decimal value to write to the location
-*
-*@return int
+/**​​
+*​​@brief​​ converts decimal equivalent of number into octal form
+​​*
+​​*​​​Decimal value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Decimal value ​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ int.​
 */
-int32_t dec2oct (int32_t dec);
+int dec2oct (int dec);
 
-/**
-*@brief converts decimal equivalent of number into hexadecimal form
-*
-*Decimal value which needs to be converted
-*
-*@param Decimal value to write to the location
-*
-*@return int
+/**​​
+*​​@brief​​ converts decimal equivalent of number into hexadecimal form
+​​*
+​​*​​​Decimal value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Decimal value ​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ int.​
 */
-int32_t dec2hex (int32_t dec);
+int dec2hex (int dec);
 
-/**
-*@brief Calculates sign 1's complement of input
-*
-*Input Decimal equivalent value which needs to be represented in sign 1's complement form
-*
-*@param Decimal equivalent of input 
-*@param op_size value to write to the location
-*
-*@return int
+/**​​
+*​​@brief​​ Calculates sign 1's complement of input
+​​*
+​​*​​​Input Decimal equivalent value which needs to be represented in sign 1's complement form​​​
+​​*
+​​*​​@param​​ Decimal equivalent of input
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ int.​
 */
-int32_t sign1comp (int32_t Decimal, int32_t op_size);
+int sign1comp (int Decimal, int op_size);
 
-/**
-*@brief converts decimal equivalent of sign 1's complement
+/**​​
+*​​@brief​​ converts decimal equivalent of sign 1's complement
 *number into binary equivalent
-*
-*Decimal equivalent value which needs to be converted
-*
-*@param Sign1complement in decimal form 
-*@param op_size value to write to the location
-*
-*@return void
+​​*
+​​*​​​Decimal equivalent value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Sign1complement in decimal form
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ void.​
 */
-void sign1comp_dec2bin (int32_t Sign1complement_dec,int32_t op_size);
+void sign1comp_dec2bin (int Sign1complement_dec,int op_size);
 
 
-/**
-*@brief converts decimal equivalent of sign 2's complement
+/**​​
+*​​@brief​​ converts decimal equivalent of sign 2's complement
 *number into binary equivalent
-*
-*Decimal equivalent value which needs to be converted
-*
-*@param Sign 2's complement in decimal form 
-*@param op_size value to write to the location
-*
-*@return void
+​​*
+​​*​​​Decimal equivalent value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Sign 2's complement in decimal form
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ void.​
 */
-void sign2comp_dec2bin (int32_t Sign2complement_dec,int32_t op_size);
+void sign2comp_dec2bin (int Sign2complement_dec,int op_size);
 
 
-/**
-*@brief Calculates signed magnitude complement of input
-*
-*Input Decimal equivalent value which needs to be represented 
-*in sign magnitude binary form
-*
-*@param Decimal equivalent of input 
-*@param op_size value to write to the location
-*
-*@return int
+/**​​
+*​​@brief​​ Calculates signed magnitude complement of input
+​​*
+​​*​​​Input Decimal equivalent value which needs to be represented
+*in sign magnitude binary form​​​
+​​*
+​​*​​@param​​ Decimal equivalent of input
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ int.​
 */
-int32_t sign_magnitude_dec (int32_t Decimal,int32_t op_size);
+int sign_magnitude_dec (int Decimal, int op_size);
 
-/**
-*@brief converts decimal equivalent of sign magnitude
+/**​​
+*​​@brief​​ converts decimal equivalent of sign magnitude
 *number into binary equivalent
-*
-*Decimal equivalent value which needs to be converted
-*
-*@param Sign magnitude in decimal form 
-*@param op_size value to write to the location
-*
-*@return void
+​​*
+​​*​​​Decimal equivalent value which needs to be converted​​​​
+​​*
+​​*​​@param​​ Sign magnitude in decimal form
+​​*​​@param​​ op_size ​value​ ​to ​​write​ ​to ​​the ​​location​
+​*
+​​*​​@return​​ void.​
 */
-void sign_mag_dec2bin (int32_t Signed_magnitude_dec,int32_t op_size);
+void sign_mag_dec2bin (int Signed_magnitude_dec,int op_size);
 
-#endif	/*	PES_PROJECT1_PROGRAM1.h */
+#endif	/*	PES_PROJECT1_PROGRAM1 */
